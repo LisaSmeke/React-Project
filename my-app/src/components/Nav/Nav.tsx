80dab1246049e39ef5cbf6ce8a106f22a73f5c67
Lisa Smeke <lisasmeke@gmail.com>
2022-09-05 14:37:50 +0200

    Add and style navbar links

--- my-app/src/components/Nav/Nav.tsx
+++ my-app/src/components/Nav/Nav.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
 import styles from './Nav.module.css';
 import logo from './ic-logo-h.svg';
+import * as data from './links.json';
+const linksString = JSON.stringify(data);
+const links = JSON.parse(linksString).links;
+
+type Link = {
+  label: string;
+  href: string;
+};
+
+const Links: React.FC<{ links: Link[] }> = ({ links }) => {
+  return (
+    <div className={styles['links-container']}>
+      {links.map((link: Link) => {
+        return (
+          <div key={link.href} className={styles['link']}>
+            <a className={styles.link} href={link.href}>
+              {link.label}
+            </a>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 const Nav: React.FC<{}> = () => {
   return (
@@ -8,6 +32,7 @@ const Nav: React.FC<{}> = () => {
       <div className={styles['logo-container']}>
         <img className={styles.logo} src={logo} alt="Impact Coding logo" />
       </div>
+      <Links links={links} />
     </nav>
   );
 };
