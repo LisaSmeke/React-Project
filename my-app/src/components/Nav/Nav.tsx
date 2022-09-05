8630e9d68bc3a0e1e0842de5d364d9ed6e85da77
Lisa Smeke <lisasmeke@gmail.com>
2022-09-05 11:51:41 +0200

    Add logo in navbar

--- my-app/src/components/Nav/Nav.tsx
+++ my-app/src/components/Nav/Nav.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './Nav.module.css';
+import logo from './ic-logo-h.svg';
 
 const Nav: React.FC<{}> = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles['logo-container']}>
-        <span>Logo</span>
+        <img src={logo} alt="Impact Coding logo" />
       </div>
     </nav>
   );
