e0774283e087b0ed696794e3ef963f47c13325e0
Lisa Smeke <lisasmeke@gmail.com>
2022-09-06 15:01:46 +0200

    Add Blog component

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from './components/Nav/Nav';
 import About from './components/pages/About/About';
+import Blog from './components/pages/Blog/Blog';
 import Footer from './components/Footer/Footer';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Nav />
       <About />
+      <Blog />
       <Footer />
     </>
   );
