47fbb67a52f1d6e0b34a0a9681fa0a617dc8cde0
Lisa Smeke <lisasmeke@gmail.com>
2022-09-22 11:53:51 +0200

    Add content to Contact section

--- my-app/src/components/pages/Contact/Contact.tsx
+++ my-app/src/components/pages/Contact/Contact.tsx
@@ -10,7 +10,32 @@ const Contact = () => {
       </header>
 
       <div className={styles['contact-body']}>
-        <h1>Feel free to reach out anytime!</h1>
+        <div className={styles['contact-info']}>
+          <h3>Questions, comments, suggestions?</h3>
+          <p>Feel free to reach out!</p>
+        </div>
+        <ul className={styles['contact-icons']}>
+          <li className={styles['tel']}>
+            <a href="tel:+46790510137">
+              <i className={styles['fa-solid fa-phone fa-xl']}></i>
+            </a>
+          </li>
+          <li className={styles['mail']}>
+            <a href="mailto:lisasmeke@gmail.com?&subject=Hi%20Lisa!...">
+              <i className={styles['fa-solid fa-at fa-xl']}></i>
+            </a>
+          </li>
+          <li className={styles['linkedin']}>
+            <a href="https://linkedin.com/in/lisa-smeke" target="_blank" rel="noreferrer">
+              <i className={styles['fa-solid fa-linkedin fa-xl']}></i>
+            </a>
+          </li>
+          <li className={styles['github']}>
+            <a href="https://github.com/LisaSmeke" target="_blank" rel="noreferrer">
+              <i className={styles['fa-solid fa-github fa-xl']}></i>
+            </a>
+          </li>
+        </ul>
       </div>
     </section>
   );
