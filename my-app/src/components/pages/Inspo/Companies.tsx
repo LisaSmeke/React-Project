f9fcdf3a782c928d705e810f11040083394b6a5f
Lisa Smeke <lisasmeke@gmail.com>
2022-09-25 13:13:11 +0200

    Add styles to company cards

--- my-app/src/components/pages/Inspo/Companies.tsx
+++ my-app/src/components/pages/Inspo/Companies.tsx
@@ -25,14 +25,15 @@ const Companies = (props: CompanyProps) => {
       <div className={styles['companies-intro']}>
         <h1>Companies using tech for good</h1>
         <h2>Get inspired with their work</h2>
+        <p>Organizations that inspire through their innovations</p>
       </div>
-      <div className={styles['company-card']}>
+      <div className={styles['company-cards-wrapper']}>
         {companies.map((company) => (
-          <div className={styles['resource-details']} key={company.id}>
-            <h3 className={styles['resource-title']}>{company.name}</h3>
-            <p className={styles['resource-by']}>{company.country}</p>
-            <p className={styles['resource-description']}>{company.description}</p>
-            <p className={styles['resource-categories']}>{company.topic}</p>
+          <div className={styles['company-card']} key={company.id}>
+            <h3 className={styles['company-name']}>{company.name}</h3>
+            {/* <p className={styles['company-country']}>Country: {company.country}</p> */}
+            <p className={styles['company-topic']}>{company.topic}</p>
+            <p className={styles['company-description']}>{company.description}</p>
           </div>
         ))}
       </div>
