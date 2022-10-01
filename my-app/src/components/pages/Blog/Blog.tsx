ad1ded7bef0e2e4cc5d2898df2da8a2f07ddd418
Lisa Smeke <lisasmeke@gmail.com>
2022-10-01 11:22:41 +0200

    Add blog div and bg

--- my-app/src/components/pages/Blog/Blog.tsx
+++ my-app/src/components/pages/Blog/Blog.tsx
@@ -11,6 +11,11 @@ const Blog = () => {
 
       <div className={styles['blog-body']}>
         <h1>Keep up with the conversation on tech and sustainability</h1>
+        <div className={styles['article-container']}>
+          <h2>Blog Post Title</h2>
+          <h3>Date goes here</h3>
+          <p>Blog content goes here</p>
+        </div>
       </div>
     </section>
   );
