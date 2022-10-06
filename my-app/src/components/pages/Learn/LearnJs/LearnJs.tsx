244a7e3d7bcff0438bafe70ff8e33e8b1cb10bb9
Lisa Smeke <lisasmeke@gmail.com>
2022-10-06 13:10:01 +0200

    Update css & js resource cards

--- my-app/src/components/pages/Learn/LearnJs/LearnJs.tsx
+++ my-app/src/components/pages/Learn/LearnJs/LearnJs.tsx
@@ -1,5 +1,6 @@
 import styles from './LearnJs.module.css';
 import ReadCounter from '../ReadCounter';
+import jsLogo from '../CustomLearn/js-color.svg';
 import * as data from './js.json';
 const jsDataString = JSON.stringify(data);
 const jsData = JSON.parse(jsDataString);
@@ -9,6 +10,7 @@ interface JsContentProps {
     videos: {
       id: string;
       category: string;
+      level: string;
       title: string;
       by: string;
       description: string;
@@ -18,6 +20,7 @@ interface JsContentProps {
     docs: {
       id: string;
       category: string;
+      level: string;
       title: string;
       by: string;
       description: string;
@@ -27,6 +30,7 @@ interface JsContentProps {
     practice: {
       id: string;
       category: string;
+      level: string;
       title: string;
       by: string;
       description: string;
@@ -40,66 +44,77 @@ const JsContent = (props: JsContentProps) => {
   const { videos, docs, practice } = props.content;
 
   return (
-    <section className={styles['resources-section']}>
-      <div className={styles['resources-container']}>
-        <h1>JavaScript Video Learning Resources</h1>
+    <section className={styles['resources']}>
+      <img className={styles['html-logo']} src={jsLogo} alt=""></img>
+      <h1>Html Video Learning Resources</h1>
+      <div className={styles['resource-cards-wrapper']}>
         {videos.map((video) => (
           <div className={styles['resource-card']} key={video.id}>
             <div className={styles['resource-header']}>
-              <h3 className={styles['resource-title']}>{video.title}</h3>
+              <h3 className={styles['resource-name']}>{video.title}</h3>
               <p className={styles['resource-by']}>{video.by}</p>
             </div>
             <div className={styles['resource-details']}>
-              <p className={styles['resource-description']}>{video.description}</p>
+              <p className={styles['resource-level']}>{video.level}</p>
             </div>
-            <div className={styles['img-container']}>
-              <iframe
-                className={styles['resource-video']}
-                title={video.title}
-                src={video.href}
-                allowFullScreen
-              ></iframe>
-            </div>
+            <a href={video.href} target="_blank" rel="noreferrer">
+              <div className={styles['video-container']}>
+                <iframe
+                  className={styles['resource-video']}
+                  title={video.title}
+                  src={video.href}
+                  allowFullScreen
+                ></iframe>
+                <p className={styles['video-resource-description']}>{video.description}</p>
+              </div>
+            </a>
           </div>
         ))}
       </div>
 
-      <div className={styles['resources-container']}>
-        <h1>JavaScript Documentation</h1>
+      <h1>Html Documentation</h1>
+      <div className={styles['resource-cards-wrapper']}>
         {docs.map((doc) => (
           <div className={styles['resource-card']} key={doc.id}>
             <div className={styles['resource-header']}>
-              <h3 className={styles['resource-title']}>{doc.title}</h3>
+              <h3 className={styles['resource-name']}>{doc.title}</h3>
               <p className={styles['resource-by']}>{doc.by}</p>
             </div>
             <div className={styles['resource-details']}>
-              <p className={styles['resource-description']}>{doc.description}</p>
+              <p className={styles['resource-level']}>{doc.level}</p>
             </div>
             <div className={styles['img-container']}>
               <a href={doc.href} target="_blank" rel="noreferrer">
-                <img className={styles['doc-img']} src={doc.img} alt=""></img>
+                <img className={styles['resource-img']} src={doc.img} alt=""></img>
+                <p className={styles['resource-description']}>{doc.description}</p>
               </a>
-              <ReadCounter />
+              <div>
+                <ReadCounter></ReadCounter>
+              </div>
             </div>
           </div>
         ))}
       </div>
 
-      <div className={styles['resources-container']}>
-        <h1>JavaScript Practice Exercises</h1>
+      <h1>Html Practice Exercises</h1>
+      <div className={styles['resource-cards-wrapper']}>
         {practice.map((practice) => (
           <div className={styles['resource-card']} key={practice.id}>
             <div className={styles['resource-header']}>
-              <h3 className={styles['resource-title']}>{practice.title}</h3>
+              <h3 className={styles['resource-name']}>{practice.title}</h3>
               <p className={styles['resource-by']}>{practice.by}</p>
             </div>
             <div className={styles['resource-details']}>
-              <p className={styles['resource-description']}>{practice.description}</p>
+              <p className={styles['resource-level']}>{practice.level}</p>
             </div>
             <div className={styles['img-container']}>
               <a href={practice.href} target="_blank" rel="noreferrer">
-                <img className={styles['practice-img']} src={practice.img} alt=""></img>
+                <img className={styles['resource-img']} src={practice.img} alt=""></img>
+                <p className={styles['resource-description']}>{practice.description}</p>
               </a>
+              <div>
+                <ReadCounter></ReadCounter>
+              </div>
             </div>
           </div>
         ))}
